ee95594f7522566527e35bb3288cbb51aa9c3ce4
khalid-elmati <elmatikhalid17@gmail.com>
2025-04-02 02:10:43 +0000

    Update category index view: Add category list and actions

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,1 +1,33 @@
-khalid el mati 
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <h2>Liste des Catégories</h2>
+        <a href="{{ route('category.create') }}" class="btn btn-primary">Ajouter une Catégorie</a>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>
+                            <a href="{{ route('category.edit', $category->id) }}" class="btn btn-warning">Modifier</a>
+                            <form action="{{ route('category.destroy', $category->id) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
