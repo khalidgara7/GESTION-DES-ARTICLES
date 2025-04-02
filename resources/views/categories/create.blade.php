9f5341b7d5af762c6363bc05871e502dbc40efa2
khalid-elmati <elmatikhalid17@gmail.com>
2025-04-02 02:10:21 +0000

    Add category creation view

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Ajouter une Catégorie</h2>
+        <form action="{{ route('category.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom de la Catégorie</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <button type="submit" class="btn btn-success">Ajouter</button>
+        </form>
+    </div>
+@endsection
