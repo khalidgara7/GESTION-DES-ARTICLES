50c9fcfb5502264cbf3beb16390557c6c30b72b0
khalid-elmati <elmatikhalid17@gmail.com>
2025-04-05 16:28:08 +0000

    Refactor comment deletion route and add comment index route for article comments

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,13 +10,24 @@
     @foreach($article->comments as $comment)
         <div class="border p-2 mb-2">
             <p>{{ $comment->description }}</p>
-            <form action="{{ route('comments.destroy', $comment->id) }}" method="POST">
-                @csrf
-                @method('POST')
+            
+            <form action="{{ route('comments.destroy', ['article_Id' => $article->id, 'comment_Id' => $comment->id]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
                 <button type="submit" class="btn btn-danger btn-sm">Supprimer</button>
             </form>
-        </div>
-    @endforeach
+
+            </div>
+        @endforeach
+
+        @section('navbar-links')
+            @isset($article)
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('comments.index', ['id' => $article->id]) }}">Comments</a>
+                </li>
+            @endisset
+        @endsection
+
 
     @include('comments.form')
 @endsection
